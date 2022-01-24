b7ee955474a3a2f2189c49ca51566e930ed5417f
Marie Rose Templonuevo <marierosetemplonuevo8@gmail.com>
2022-01-24 15:35:39 +0800

    result

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const xClass = "x";
 const oClass = "o";
-
+const currentTurn = document.getElementById("turn");
+const header = document.getElementById("header");
+// const winnerMessage = currentClass + "'s wins";
 const cells = document.querySelectorAll("[data-cell]");
+
 const winnerPattern = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,7 +15,7 @@ const winnerPattern = [
   [0, 4, 8],
   [2, 4, 6]
 ];
-let oTurn;
+let playersTurn;
 
 cells.forEach(cell => {
   cell.addEventListener("click", turn, { once: true });
@@ -20,20 +23,22 @@ cells.forEach(cell => {
 
 function turn(e) {
   const cell = e.target;
-  const currentClass = oTurn ? oClass : xClass;
+  const currentClass = playersTurn ? oClass : xClass;
   placeMark(cell, currentClass);
   if (checkWinner(currentClass)) {
-    alert("The winner is " + currentClass);
+    header.innerHTML = "The winner is " + currentClass;
+    // alert("The winner is " + currentClass);
   }
   swapTurns();
 }
 
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass);
+  currentTurn.classList.add(currentClass);
 }
 
 function swapTurns() {
-  oTurn = !oTurn;
+  playersTurn = !playersTurn;
 }
 
 function checkWinner(currentClass) {
